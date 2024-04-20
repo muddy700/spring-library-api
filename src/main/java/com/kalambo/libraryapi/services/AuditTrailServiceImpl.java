faec025bd3dd86f67710f68ddd6226c30f5297bd
Mohamed Mfaume Mohamed <mohamed.mohamed5@tra.go.tz>
2024-04-20 11:01:55 +0300

    fix(Auth): Resolve dependency cycle caused by authService in auditTrail

--- src/main/java/com/kalambo/libraryapi/services/AuditTrailServiceImpl.java
+++ src/main/java/com/kalambo/libraryapi/services/AuditTrailServiceImpl.java
@@ -4,10 +4,15 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.kalambo.libraryapi.dtos.AuditTrailDto;
 import com.kalambo.libraryapi.entities.AuditTrail;
+import com.kalambo.libraryapi.entities.User;
 import com.kalambo.libraryapi.exceptions.ResourceNotFoundException;
 import com.kalambo.libraryapi.mappers.PageMapper;
 import com.kalambo.libraryapi.mappers.AuditTrailMapper;
@@ -19,9 +24,6 @@ import com.kalambo.libraryapi.responses.IAuditTrailV2;
 @Service
 public class AuditTrailServiceImpl implements AuditTrailService {
     @Autowired
-    private AuthService authService;
-
-    @Autowired
     private AuditTrailRepository auditTrailRepository;
 
     @Autowired
@@ -32,7 +34,7 @@ public class AuditTrailServiceImpl implements AuditTrailService {
 
     @Override
     public void create(AuditTrailDto payload) {
-        auditTrailRepository.save(payload.toEntity().setUser(authService.getPrincipal()));
+        auditTrailRepository.save(payload.toEntity().setUser(getPrincipal()));
     }
 
     @Override
@@ -50,5 +52,15 @@ public class AuditTrailServiceImpl implements AuditTrailService {
         String errorMessage = "No audit-trail found with ID: " + trailId;
 
         return auditTrailRepository.findById(trailId).orElseThrow(() -> new ResourceNotFoundException(errorMessage));
+    }
+
+    // TODO: Remove this function and reuse from authService
+    private User getPrincipal() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth instanceof AnonymousAuthenticationToken)
+            throw new AccessDeniedException("Request not authenticated");
+
+        return (User) auth.getPrincipal();
     }
 }
